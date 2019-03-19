6f153cbd4189f134f90933d9d1a8383eecb8ab91
andrewtanggara <>
2019-03-20 08:54:23 +1100

    branch init

--- Debate_Night-Assignment/DataAssessment.R
+++
@@ -1,8 +0,0 @@
-dataset <- read.csv("./data/sample_users_100k.csv",sep="\t",stringsAsFactors = F)
-
-test_set <- read.csv("./data/testing_set_features.csv",sep="\t",stringsAsFactors = F)
-View(test_set)
-
-example_res <- read.csv("./data/EXAMPLE_predictions_uni1_uni2_uni3.csv",sep="\t",stringsAsFactors = F)
-View(example_res)
-
